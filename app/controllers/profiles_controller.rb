e279f8050b3f12fc68f4d4ead6e5ae6ac3f23abb
thesebonesalone <duikhead2@gmail.com>
2020-10-19 20:15:03 -0500

    Moving to Will

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-
+before_action :current_user
 
 def show
   @profile = Profile.find(params[:id])
@@ -43,6 +43,9 @@ private
 
 def profile_params 
   params.require(:profile).permit(:name, :user_id)
+end
+def current_user
+  @current_user ||= session[:user_id] && User.find(session[:user_id])
 end
 
 end
