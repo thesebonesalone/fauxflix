af4f581a9a46a4bcf7cd9a8e67bf998b73e9099a
thesebonesalone <duikhead2@gmail.com>
2020-10-22 13:32:01 -0500

    Fixed database loading error

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,13 @@ class WelcomeController < ApplicationController
   
   def home
     session[:return_to] ||= "/"
+
+    Movie.all.each do |movie|
+      if movie.img == nil
+        @info = TmdbApi.new(movie.title).build_info
+        movie.update(description: @info[:description], img: @info[:image], backdrop: @info[:backdrop])
+      end
+    end
   end
 
   def testbg
